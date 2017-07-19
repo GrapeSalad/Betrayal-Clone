ff77161d58cc89fb818f1ce7eaf6138669fa3cbb
James Lannon and Karina Roush <james+karina@>
2017-07-19 15:09:46 -0700

    we got neat to work! 🙏

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { WelcomeComponent } from './welcome/welcome.component';
+
 import {routing} from './app.routing';
 import { AppComponent } from './app.component';
-import { CharacterInfoComponent } from './character-info/character-info.component';
-import { EnterComponent } from './enter/enter.component';
-import { LeftRoomComponent } from './left-room/left-room.component';
-import { RightRoomComponent } from './right-room/right-room.component';
-import { KitchenComponent } from './kitchen/kitchen.component';
-import { GardenComponent } from './garden/garden.component';
-import { MusicRoomComponent } from './music-room/music-room.component';
-import { LibraryComponent } from './library/library.component';
+
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CharacterSelectComponent } from './character-select/character-select.component';
 import { GameBoardComponent } from './game-board/game-board.component';
@@ -20,15 +13,6 @@ import { GameBoardComponent } from './game-board/game-board.component';
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    CharacterInfoComponent,
-    EnterComponent,
-    LeftRoomComponent,
-    RightRoomComponent,
-    KitchenComponent,
-    GardenComponent,
-    MusicRoomComponent,
-    LibraryComponent,
     LandingPageComponent,
     CharacterSelectComponent,
     GameBoardComponent
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,52 +1,10 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-import {CharacterInfoComponent} from './character-info/character-info.component';
-import { EnterComponent } from './enter/enter.component';
-import { LeftRoomComponent } from './left-room/left-room.component';
-import { RightRoomComponent } from './right-room/right-room.component';
-import { GardenComponent } from './garden/garden.component';
-import { MusicRoomComponent } from './music-room/music-room.component';
-import { LibraryComponent } from './library/library.component';
-import { KitchenComponent } from './kitchen/kitchen.component';
+
+
 
 const appRoutes: Routes = [
-  {
-    path: '',
-    component: WelcomeComponent
-  },
-  {
-    path: 'character-info',
-    component: CharacterInfoComponent
-  },
-  {
-    path: 'enter',
-    component: EnterComponent
-  },
-  {
-    path: 'enter/leftRoom',
-    component: LeftRoomComponent
-  },
-  {
-    path: 'enter/rightRoom',
-    component: RightRoomComponent
-  },
-  {
-    path: 'kitchen',
-    component: KitchenComponent
-  },
-  {
-    path: 'library',
-    component: LibraryComponent
-  },
-  {
-    path: 'garden',
-    component: GardenComponent
-  },
-  {
-    path: 'music-room',
-    component: MusicRoomComponent
-  }
+  
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
