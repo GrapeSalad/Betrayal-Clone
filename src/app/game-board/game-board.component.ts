1717bde765ed375cc49c7ebebee8e282dc6cdc5f
David Wilson <davidtheadmiral@gmail.com>
2017-07-22 19:47:04 -0700

    fiddling with html to better facilitate movement and maybe even tile display.

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import {Character} from '../character.model';
 import {Room} from '../room.model';
 import {Router} from '@angular/router';
@@ -9,8 +9,11 @@ import { FirebaseListObservable } from 'angularfire2/database';
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.scss'],
+  host: {'(document:keydown)': 'handleKeyboardEvent($event)'},
   providers: [GameService]
 })
+
+
 export class GameBoardComponent implements OnInit {
   staticRoomTiles;
   roomTiles;
@@ -26,65 +29,80 @@ export class GameBoardComponent implements OnInit {
   chosenEvent;
   chosenOmen;
   chosenRoom;
-
+  key;
   constructor(private gameService: GameService) { }
+
+  // @HostListener('document:keypress',['$event'])
+  handleKeyboardEvent(event: KeyboardEvent){
+    this.key = event.which || event.keyCode;
+    console.log(this.key);
+    if(this.key === 13){
+      //add class active to entrance hall div(should be static div)
+    }
+    else if(this.key === 37){
+      //get active div id
+      //remove active class from div id
+      //add active class to div on the left eg:(#7) from (#8)
+    }
+  }
 
   ngOnInit() {
     this.gameService.getStaticRoomTiles().subscribe(dataLastEmittedFromObserver => {
       this.staticRoomTiles = dataLastEmittedFromObserver;
-      this.entranceHall = dataLastEmittedFromObserver[2];
-      this.foyer = dataLastEmittedFromObserver[3];
-      this.grandStaircase = dataLastEmittedFromObserver[4];
+      this.entranceHall = dataLastEmittedFromObserver[0];
+      this.foyer = dataLastEmittedFromObserver[1];
+      this.grandStaircase = dataLastEmittedFromObserver[2];
+      // console.log(this.entranceHall.src);
     })
-    this.gameService.getRoomTiles().subscribe(dataLastEmittedFromObserver => {
-      this.roomTiles = dataLastEmittedFromObserver;
-      for(var i=0; i < dataLastEmittedFromObserver.length; i++) {
-        this.roomTileArr.push(new Room(dataLastEmittedFromObserver[i].name, dataLastEmittedFromObserver[i].basement, dataLastEmittedFromObserver[i].ground, dataLastEmittedFromObserver[i].upper, dataLastEmittedFromObserver[i].eventCard, dataLastEmittedFromObserver[i].omenCard, dataLastEmittedFromObserver[i].itemCard, dataLastEmittedFromObserver[i].topDoor, dataLastEmittedFromObserver[i].bottomDoor, dataLastEmittedFromObserver[i].leftDoor, dataLastEmittedFromObserver[i].rightDoor, dataLastEmittedFromObserver[i].text, dataLastEmittedFromObserver[i].src));
-      }
-      while(this.roomTileArr.length > 0){
-        var randomNumber = this.gameService.getRandomNumber(0, (this.roomTileArr.length-1));
-        // console.log(this.roomTileArr.length);
-        // while(this.roomTileArr.length > 4) {
-        //   console.log("hi");
-        // }
-        var removed = this.roomTileArr.splice(randomNumber, 1);
-        if(removed[0].ground){
-          this.randomGroundRoomTiles.push(removed);
-        }
-        else if(removed[0].basement){
-            this.randomBasementRoomTiles.push(removed);
-        }
-        else{
-          this.randomUpperRoomTiles.push(removed);
-        }
-      }
-      //trying to test left/right pathing
-      for(i=0; i<this.randomGroundRoomTiles.length; i++) {
-        if(this.randomGroundRoomTiles[i][0].rightDoor && this.randomGroundRoomTiles[i+1][0].leftDoor){
-          this.rightToLeftDoorArray.push(this.randomGroundRoomTiles[i]);
-          for(var k=0;k<this.rightToLeftDoorArray.length; k++){
-            if(this.randomGroundRoomTiles[i+1][0] === this.rightToLeftDoorArray[k][0]){
-              this.rightToLeftDoorArray.push(this.randomGroundRoomTiles[i+1][0]);
-            }
-          }
-        }
-        else{
-          console.log("not yay");
-        }
-      }
-      console.log(this.rightToLeftDoorArray);
-
-    })
-    this.gameService.getEventCardById(this.gameService.getRandomNumber(0,24)).subscribe(dataLastEmittedFromObserver => {
-      this.chosenEvent = dataLastEmittedFromObserver;
-    })
-    this.gameService.getOmenCardById(this.gameService.getRandomNumber(0,7)).subscribe(dataLastEmittedFromObserver => {
-      this.chosenOmen = dataLastEmittedFromObserver;
-    })
-    this.gameService.getRoomTileById(this.gameService.getRandomNumber(0,24)).subscribe(dataLastEmittedFromObserver => {
-      this.chosenRoom = dataLastEmittedFromObserver;
-    })
-
+  //   this.gameService.getRoomTiles().subscribe(dataLastEmittedFromObserver => {
+  //     this.roomTiles = dataLastEmittedFromObserver;
+  //     for(var i=0; i < dataLastEmittedFromObserver.length; i++) {
+  //       this.roomTileArr.push(new Room(dataLastEmittedFromObserver[i].name, dataLastEmittedFromObserver[i].basement, dataLastEmittedFromObserver[i].ground, dataLastEmittedFromObserver[i].upper, dataLastEmittedFromObserver[i].eventCard, dataLastEmittedFromObserver[i].omenCard, dataLastEmittedFromObserver[i].itemCard, dataLastEmittedFromObserver[i].topDoor, dataLastEmittedFromObserver[i].bottomDoor, dataLastEmittedFromObserver[i].leftDoor, dataLastEmittedFromObserver[i].rightDoor, dataLastEmittedFromObserver[i].text, dataLastEmittedFromObserver[i].src));
+  //     }
+  //     while(this.roomTileArr.length > 0){
+  //       var randomNumber = this.gameService.getRandomNumber(0, (this.roomTileArr.length-1));
+  //       // console.log(this.roomTileArr.length);
+  //       // while(this.roomTileArr.length > 4) {
+  //       //   console.log("hi");
+  //       // }
+  //       var removed = this.roomTileArr.splice(randomNumber, 1);
+  //       if(removed[0].ground){
+  //         this.randomGroundRoomTiles.push(removed);
+  //       }
+  //       else if(removed[0].basement){
+  //           this.randomBasementRoomTiles.push(removed);
+  //       }
+  //       else{
+  //         this.randomUpperRoomTiles.push(removed);
+  //       }
+  //     }
+  //     //trying to test left/right pathing
+  //     for(i=0; i<this.randomGroundRoomTiles.length; i++) {
+  //       if(this.randomGroundRoomTiles[i][0].rightDoor && this.randomGroundRoomTiles[i+1][0].leftDoor){
+  //         this.rightToLeftDoorArray.push(this.randomGroundRoomTiles[i]);
+  //         for(var k=0;k<this.rightToLeftDoorArray.length; k++){
+  //           if(this.randomGroundRoomTiles[i+1][0] === this.rightToLeftDoorArray[k][0]){
+  //             this.rightToLeftDoorArray.push(this.randomGroundRoomTiles[i+1][0]);
+  //           }
+  //         }
+  //       }
+  //       else{
+  //         console.log("not yay");
+  //       }
+  //     }
+  //     console.log(this.rightToLeftDoorArray);
+  //
+  //   })
+  //   this.gameService.getEventCardById(this.gameService.getRandomNumber(0,24)).subscribe(dataLastEmittedFromObserver => {
+  //     this.chosenEvent = dataLastEmittedFromObserver;
+  //   })
+  //   this.gameService.getOmenCardById(this.gameService.getRandomNumber(0,7)).subscribe(dataLastEmittedFromObserver => {
+  //     this.chosenOmen = dataLastEmittedFromObserver;
+  //   })
+  //   this.gameService.getRoomTileById(this.gameService.getRandomNumber(0,24)).subscribe(dataLastEmittedFromObserver => {
+  //     this.chosenRoom = dataLastEmittedFromObserver;
+  //   })
+  //
   }
 
 }
