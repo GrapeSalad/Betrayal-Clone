8903acddc1a8639899092152528afd55d6c41122
Alyssa Moody and David Wilson <alyssa+david@>
2017-07-24 09:36:50 -0700

    Add character service. Add ability to pull fromm firebase for character select page.

--- src/app/character-select/character-select.component.ts
+++ src/app/character-select/character-select.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 // import { FriendSelectComponent } from './friend-select/friend-select.component';
+import { Character } from '../character.model';
+import { CharacterService } from '../character.service';
+
+import { Router } from '@angular/router';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-character-select',
   templateUrl: './character-select.component.html',
-  styleUrls: ['./character-select.component.scss']
+  styleUrls: ['./character-select.component.scss'],
+  providers: [CharacterService]
 })
 export class CharacterSelectComponent implements OnInit {
+  characters: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private characterService: CharacterService) { }
 
   ngOnInit() {
+    this.characters = this.characterService.getCharacters();
+    console.log(this.characters);
   }
+
+  goToFriendPage(clickedCharacter){
+    this.router.navigate(['characters', clickedCharacter.$key]);
+  };
 
 }
